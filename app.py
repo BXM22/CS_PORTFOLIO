ed15c231b55f3003e7441002e21d934351b9cabb
BXM22 <brennen@csuf.fullerton.edu>
2025-03-26 00:38:41 -0700

    Light edit
    Change author

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -10,5 +10,12 @@ def home():
 def get_data():
     return {"key": "value"}  # Example API endpoint
 
+@app.route('/contact', methods=['POST'])
+def contact():
+    name = request.form.get('name')
+    email = request.form.get('email')
+    # Send email (use Flask-Mail)
+    return "Message sent!"
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000, debug=True)
+    app.run(host="0.0.0.0", port=8002, debug=True)
